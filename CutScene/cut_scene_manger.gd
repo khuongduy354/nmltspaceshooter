db0060cc32be88ec92f0577dde3145c5309012a6
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-11-28 14:09:15 +0700

    feat: transition fade

--- CutScene/cut_scene_manger.gd
+++ CutScene/cut_scene_manger.gd
@@ -1,43 +1,38 @@
 extends Node2D
 
 @onready var EarthOnly = preload("res://CutScene/EarthOnly.tscn")
-@onready var HumanWar = preload("res://CutScene/human_war.tscn")
-@onready var KeplerAliensAttack = preload("res://CutScene/keplers_alien_attack.tscn")
 @onready var KeplerMar = preload("res://CutScene/kepler_and_mars.tscn")
-@onready var KeplerWithAlien = preload("res://CutScene/kepler_with_aliens.tscn")
+@onready var HumanWar = preload("res://CutScene/human_war.tscn")
 @onready var KeplerRiftOpening = preload("res://CutScene/rift_opening.tscn")
-
+@onready var KeplerAliensAttack = preload("res://CutScene/keplers_alien_attack.tscn")
+@onready var KeplerWithAlien = preload("res://CutScene/kepler_with_aliens.tscn")
 @onready var world1rift = preload("res://CutScene/world_1_rift.tscn")
+
+@onready var trans = $SwipeTransistion
+
+func play_sequential_cutscenes(scenes: Array[PackedScene],times: Array[int]): 
+	if scenes.size() != times.size(): 
+		print("ERROR: unmatched scene and its duration")
+		return 
+	var idx = 0 
+	for scene in scenes: 
+		
+		var node = scene.instantiate()
+		add_child(node)
+		trans.animp.play("open")
+		await trans.opened
+		
+		await wait(times[idx])
+		
+		trans.animp.play("close")
+		await trans.closed
+		node.queue_free()
+		
+		idx += 1 
+
 func pregame_flow(): 
-	var curr_scene = EarthOnly.instantiate()
-	add_child(curr_scene)
-	await wait(5)
-	curr_scene.queue_free()
-	
-	curr_scene  = KeplerMar.instantiate()	
-	add_child(curr_scene)
-	await wait(5)
-	curr_scene.queue_free()
-	
-	curr_scene  = HumanWar.instantiate()	
-	add_child(curr_scene)
-	await wait(5)
-	curr_scene.queue_free()
+	play_sequential_cutscenes([EarthOnly,KeplerMar,HumanWar,KeplerRiftOpening,KeplerAliensAttack,KeplerWithAlien,world1rift],[5,5,5,10,8,5,5])
 
-	curr_scene  = KeplerRiftOpening.instantiate()	
-	add_child(curr_scene)
-	await wait(10)
-	curr_scene.queue_free()
-
-	curr_scene  = KeplerAliensAttack.instantiate()	
-	add_child(curr_scene)
-	await wait(8)
-	curr_scene.queue_free()
-
-	curr_scene  = KeplerWithAlien.instantiate()	
-	add_child(curr_scene)
-	await wait(5)
-	curr_scene.queue_free()
 	
 func wait(t): 
 	await get_tree().create_timer(t).timeout
---
+++ vfx/SwipeTransistion.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+
+@onready var animp = $AnimationPlayer
+var file  = null 
+
+signal opened 
+signal closed
+
+func transition_to(_file: String):
+	file = _file
+	animp.play("close")
+
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "close":
+		closed.emit()
+	if anim_name == "open": 
+		opened.emit() 
